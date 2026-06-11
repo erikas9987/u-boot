7733ad5b6d4efe552309b7b9afa194409e680c7b
Erikas Bitovtas <xerikasxx@gmail.com>
2026-06-11 16:10:22 +0300

    serial: framebuffer: adjust the resolution for Samsung devices
    Needed so serial can be seen properly on Samsung Galaxy A52/A72.
    
    Signed-off-by: Erikas Bitovtas <xerikasxx@gmail.com>

--- drivers/serial/serial_framebuffer.c
+++ drivers/serial/serial_framebuffer.c
@@ -14,8 +14,8 @@
 #include <errno.h>
 #include "font.h"
 
-#define SCREEN_WIDTH 2944
-#define SCREEN_HEIGHT 1840
+#define SCREEN_WIDTH 1080
+#define SCREEN_HEIGHT 2400
 #define COL_WIDTH 80 // In characters
 
 DECLARE_GLOBAL_DATA_PTR;
